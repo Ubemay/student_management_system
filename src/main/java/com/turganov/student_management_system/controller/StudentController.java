2fa70dd349545c8f8a54efe98497ac83504ceec4
Rustambek Turganov <aaa_ppp_1989@mail.ru>
2023-09-19 20:07:35 +0600

    adding method getCurrentUser() in repository

--- src/main/java/com/turganov/student_management_system/controller/StudentController.java
+++ src/main/java/com/turganov/student_management_system/controller/StudentController.java
@@ -4,6 +4,8 @@ import com.turganov.student_management_system.entity.Student;
 import com.turganov.student_management_system.service.StudentService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -95,6 +97,22 @@ public class StudentController {
     public ResponseEntity<List<Student>> filterStudentsByAgeRange(@RequestParam(required = false) Integer minAge, @RequestParam(required = false) Integer maxAge) {
         List<Student> filteredStudents = studentService.filterStudentsByAgeRange(minAge, maxAge);
         return ResponseEntity.ok(filteredStudents);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<Student> getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated()) {
+            String username = authentication.getName();
+            Student student = studentService.findStudentsByName(username);
+
+            if (student != null) {
+                return new ResponseEntity<>(student, HttpStatus.OK);
+            }
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
